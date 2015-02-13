ab49e35dfa1e73bb45c85dfd0b77801b52b3e75c
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-02-13 12:22:03 +0200

    First commit!

---
+++ TeamWork/DevilGame/DevilInTheSky/Devil.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+namespace DevilInTheSky
+{
+    class Devil
+    {
+
+  static public char[,] turnUp = new char[8, 8]{{' ',' ',' ','*',' ','*',' ',' '},
+                                                {' ',' ','*','*',' ','*','*',' '},
+                                                {' ',' ','*','*',' ','*','*',' '},
+                                                {' ',' ','*','*','*','*','*',' '},
+                                                {' ',' ','*','*','*','*','*',' '},
+                                                {' ',' ','*','*','*','*','*',' '},
+                                                {' ',' ','*','*','*','*','*',' '},
+                                                {' ',' ',' ','*','*','*',' ',' '}};
+
+ static public char[,] turnUpRight = new char[8, 8]{{' ',' ',' ','*',' ','*',' ',' '},
+                                                    {' ',' ','*','*','*',' ',' ',' '},
+                                                    {' ','*','*','*',' ',' ',' ','*'},
+                                                    {'*','*','*','*','*',' ','*','*'},
+                                                    {'*','*','*','*','*','*','*','*'},
+                                                    {'*','*','*','*','*','*','*',' '},
+                                                    {' ','*','*','*','*','*',' ',' '},
+                                                    {' ',' ',' ','*',' ',' ',' ',' '}
+                                                    };
+        public char[,] imageDevil = turnUp;
+        public Point position = new Point(30, 30);// Start position for the devil
+        public Color color = Color.Red; // Devil in  red color 
+        public int direction = 0; // move direction (0-up,1-down,2-right,3-left,4-up right,5-up left,6-down right,7-down left
+
+
+
+
+        public void moveDevil(int nextPosoka)
+        {
+            if (direction != nextPosoka)
+            {
+                imageDevil = getImage(nextPosoka); // get the right image 
+            }
+            switch (nextPosoka)
+            {
+                case 0:
+                    {
+                        position.Y--;
+                        break;
+                    }
+                case 1:
+                    {
+                        position.Y++;
+                        break;
+                    }
+                case 2:
+                    {
+                        position.X+=2;
+                        break;
+                    }
+                case 3:
+                    {
+                        position.X-=2;
+                        break;
+                    }
+                case 4:
+                    {
+                        position.Y--;
+                        position.X++;
+                        break;
+                    }
+                case 5:
+                    {
+                        position.Y--;
+                        position.X--;
+                        break;
+                    }
+                case 6:
+                    {
+                        position.Y++;
+                        position.X++;
+                        break;
+                    }
+                case 7:
+                    {
+                        position.Y++;
+                        position.X--;
+                        break;
+                    }
+            }
+
+        }
+
+        public char[,] getImage(int posoka)
+        {
+            switch (posoka)
+            {
+                case 0:
+                    {
+                        direction = 0;
+                        return turnUp;
+                        break;
+                    }
+                case 4:
+                    {
+                        direction = 4;
+                        return turnUpRight;
+                        break;
+
+                    }
+                case 1:
+                    {
+                        char[,] pom = new char[8, 8];
+                        for (int i = 0; i < 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom[7 - i, j] = turnUp[i, j];
+                            }
+                        direction = 1;
+                        return pom;
+
+
+                    }
+                case 2:
+                    {
+
+                        char[,] pom = new char[8, 8];
+
+                        for (int i = 0; i< 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom[j, 7-i] = turnUp[i, j];
+                            }
+                        direction = 2;
+                        return pom;
+
+                    }
+                case 3:
+                    {
+
+                        char[,] pom = new char[8, 8];
+
+                        for (int i = 0; i < 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom[j, i] = turnUp[i, j];
+                            }
+                        direction = 3;
+                        return pom;
+
+                    }
+                case 5:
+                    {
+
+                        char[,] pom = new char[8, 8];
+
+                        for (int i = 0; i < 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom[7-j, i] = turnUpRight[i, j];
+                            }
+                        direction = 5;
+                        return pom;
+
+                    }
+                case 6:
+                    {
+                        char[,] pom = new char[8, 8];
+                        for (int i = 0; i < 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom[7 - i, j] = turnUpRight[i, j];
+                            }
+                        direction = 6;
+                        return pom;
+
+                    }
+                case 7:
+                    {
+                        char[,] pom = new char[8, 8];
+                        for (int i = 0; i < 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom[7 - i, j] = turnUpRight[i, j];
+                            }
+
+                        char[,] pom2 = new char[8, 8];
+
+
+                        for (int i = 0; i < 8; i++)
+                            for (int j = 0; j < 8; j++)
+                            {
+                                pom2[j, 7 - i] = pom[i, j];
+                          }
+                        direction = 7;
+                        return pom2;
+
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+
+
+        }
+
+
+    }
+}
---
+++ TeamWork/DevilGame/DevilInTheSky/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading;
+using System.Drawing;
+namespace DevilInTheSky
+{
+    class Program
+    {
+        static void Main()
+        {
+            Devil k = new Devil();
+            int direction = 0;
+
+            //  Set Console settings  
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BufferHeight = Console.WindowHeight = 30;
+            Console.BufferWidth = Console.WindowWidth = 120;
+
+
+
+
+            string l = "";
+
+            while (true)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    if (i == 1 || i == 29)
+                        Console.WriteLine(new string('-', Console.WindowWidth -30));
+
+
+                    if (i >= k.position.Y && i <= k.position.Y + 7)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            l += k.imageDevil[i - k.position.Y, j].ToString();
+                        }
+
+                        Console.WriteLine("{1}{2}{3}", "|", new string(' ', k.position.X), l, new string(' ', Console.WindowWidth-30 - k.position.X - 7));
+                        l = "";
+                    }
+                    else
+                        Console.WriteLine("|" + new string(' ',Console.WindowWidth - 32) + "|");
+                }
+
+
+                ConsoleKeyInfo secondPressedKey = new ConsoleKeyInfo();
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo firstPressedKey = Console.ReadKey(true);
+                    while (Console.KeyAvailable)
+                    {
+                        secondPressedKey = Console.ReadKey(true);
+                    }
+
+                    direction = changeDirection(firstPressedKey, secondPressedKey);
+                    k.moveDevil(direction);
+
+                }
+                else
+                {
+
+                    k.moveDevil(direction);
+                }
+
+
+                Thread.Sleep(200);
+                Console.Clear();
+            }
+        }
+
+
+
+        static int changeDirection(ConsoleKeyInfo pressedKey, ConsoleKeyInfo pressedSecondKey)
+        {
+            if (pressedKey.Key == ConsoleKey.LeftArrow && pressedSecondKey.Key == 0)
+            {
+                return 3;
+            }
+            else
+                if (pressedKey.Key == ConsoleKey.LeftArrow && pressedSecondKey.Key == ConsoleKey.UpArrow)
+                {
+                    return 5;
+                }
+                else
+                    if (pressedKey.Key == ConsoleKey.LeftArrow && pressedSecondKey.Key == ConsoleKey.DownArrow)
+                    {
+                        return 7;
+                    }
+
+
+            if (pressedKey.Key == ConsoleKey.RightArrow && pressedSecondKey.Key == 0)
+            {
+                return 2;
+            }
+            else
+                if (pressedKey.Key == ConsoleKey.RightArrow && pressedSecondKey.Key == ConsoleKey.UpArrow)
+                {
+                    return 4;
+                }
+                else
+                    if (pressedKey.Key == ConsoleKey.RightArrow && pressedSecondKey.Key == ConsoleKey.DownArrow)
+                    {
+                        return 6;
+                    }
+            if (pressedKey.Key == ConsoleKey.UpArrow && pressedSecondKey.Key == 0)
+            {
+                return 0;
+            }
+            else
+                if (pressedKey.Key == ConsoleKey.UpArrow && pressedSecondKey.Key == ConsoleKey.LeftArrow)
+                {
+                    return 5;
+                }
+                else
+                    if (pressedKey.Key == ConsoleKey.UpArrow && pressedSecondKey.Key == ConsoleKey.RightArrow)
+                    {
+                        return 4;
+                    }
+            if (pressedKey.Key == ConsoleKey.DownArrow && pressedSecondKey.Key == 0)
+            {
+                return 1;
+            }
+            else
+                if (pressedKey.Key == ConsoleKey.DownArrow && pressedSecondKey.Key == ConsoleKey.RightArrow)
+                {
+                    return 6;
+                }
+                else
+                    if (pressedKey.Key == ConsoleKey.DownArrow && pressedSecondKey.Key == ConsoleKey.LeftArrow)
+                    {
+                        return 7;
+                    }
+            return 0;
+        }
+
+    }
+}
