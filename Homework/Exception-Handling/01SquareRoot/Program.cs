290a61f947a218a6dec4e94b59bec10e3b08b8e0
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-03-22 22:27:47 +0200

    First Commit!

---
+++ Homework/Exception-Handling/01SquareRoot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01SquareRoot
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int k = int.Parse(Console.ReadLine());
+               double sqrt = Math.Sqrt(k);
+                if (k <0)
+                    throw new ArgumentOutOfRangeException();
+                Console.WriteLine(sqrt);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Invalid Number!" + e.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+
+        }
+    }
+}
---
+++ Homework/Exception-Handling/02EnterNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10;i++)
+            {
+                ReadNumber(1, 100);
+            }
+        }
+    
+        static void ReadNumber(int start, int end)
+        {
+
+            try{
+                int k  = int.Parse(Console.ReadLine());
+                if(start >=k || k>=end)
+                {
+                    throw new ArgumentOutOfRangeException();
+
+                }
+            }
+            catch(Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+        }   
+    
+    }
+}
